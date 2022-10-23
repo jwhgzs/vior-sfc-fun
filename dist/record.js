3ddf9240fcf265268a2e6dafd4bb5531b6fb4829
jwhgzs <admin@jwhgzs.com>
2022-10-23 12:49:21 +0800

    v0.4.0

--- dist/record.js
+++ dist/record.js
@@ -1,6 +1,6 @@
 
-        import origin from './_record.js'
-        origin.html = ` <style $ownstyle>
+                import origin from './_record.js'
+                origin.html = ` <style $ownstyle>
         li {
             list-style-type: none;
         }
@@ -10,5 +10,5 @@
             border-bottom: 1px solid #eee;
         }
     </style> <li> <h3>{{ record }}</h3> <div class="bottom"> <small>{{ time }}</small> <br/> <small>{{ scramble }}</small> <br/> <slot-receiver></slot-receiver> </div> </li> `
-        export default origin
-    
+                export default origin
+            
